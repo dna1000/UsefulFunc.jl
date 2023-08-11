e0589733613ea25ba4c76370747c0ad045ff4d61
dna1000 <47078590+dna1000@users.noreply.github.com>
2023-08-11 11:56:24 +0100

    Added module docstring

--- src/UsefulFunc.jl
+++ src/UsefulFunc.jl
@@ -1,3 +1,19 @@
+"""
+    UsefulFunc
+
+This package provides some useful functions that are primarily
+used to as a test of julia coding and also building packages.
+
+Current functions include:
+    hex
+    hexs
+    dec
+    decs
+    flt
+    bins
+    octs
+    helloworld
+"""
 module UsefulFunc
 
 # Write your package code here.
